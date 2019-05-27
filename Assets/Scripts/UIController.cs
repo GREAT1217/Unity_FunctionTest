b7d3a75b298af909eef77b7ae458077f36bb5dd5
史行行 <tenstars@foxmail.com>
2019-05-27 10:39:25 +0800

    增加doTween插件和测试场景

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController Instance;
+
+    public UIFirstLevel _firstLevel;
+    public UISecondLevel _secondLevel;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+
+
+
+}
---
+++ Assets/Scripts/UISecondLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISecondLevel : MonoBehaviour
+{
+    public UISecondPanel _panelPrefab;
+
+    public void ShowPanel(string text)
+    {
+        _panelPrefab.InitUIPath(text);
+        _panelPrefab.MovePanel(true);
+    }
+}
