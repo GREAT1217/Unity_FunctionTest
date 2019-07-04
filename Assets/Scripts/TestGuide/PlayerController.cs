3fc69125ed988c2dd299103981b2bf85e1dcfca1
史行行 <tenstars@foxmail.com>
2019-07-04 20:16:51 +0800

    修改引导的player

--- Assets/Scripts/TestGuide/PlayerController.cs
+++ Assets/Scripts/TestGuide/PlayerController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float _speed = 30f;
     public Rigidbody _rigid;
-    float h, v;
+    public Transform _camera;
+
     void Update()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-        _rigid.MovePosition(transform.position + new Vector3(h, 0, v));
+        Vector3 targetPos = transform.position;
+        targetPos += transform.right * Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        targetPos += transform.forward * Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        _rigid.MovePosition(targetPos);
+        _rigid.MoveRotation(Quaternion.Euler(Vector3.up * _camera.eulerAngles.y));
     }
 }
